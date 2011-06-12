c90da4bff5d8f966869056cdc1d4c802760b77ad
Lordron <worldofwarcraftcataclysmmail@gmail.com>
2011-06-12 21:19:52 +0300

    Initial

---
+++ TableComparator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql;
+
+namespace TableComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Table Comparator v1.0";
+            AppDomain.CurrentDomain.UnhandledException +=
+                (o, e) => CrashReport(e.ExceptionObject.ToString());
+        }
+
+        static void CrashReport(string message)
+        {
+            Console.WriteLine("<<ERROR>>");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
